a2094e4fe01e9a01278421ea6e7c8eeef3e6f931
Matt Cherry <mttchrry@gmail.com>
2015-01-16 16:37:21 -0500

    Got those bugs Moving!

--- js/app.js
+++ js/app.js
@@ -28,6 +28,20 @@ var Enemy = function() {
 Enemy.prototype = Object.create(Actor.prototype);
 Enemy.prototype.constructor = Enemy;
 
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    if( this.x > ctx.canvas.width - Resources.get(this.sprite).width) {
+        this.x = 0;
+    }
+    else {
+        console.log( ctx.canvas.width + " and sprite = " + Resources.get(this.sprite).width)
+        this.x = this.x + (40 *dt);
+    }
+}
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
